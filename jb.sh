240ab28e5cb11160b8f17d2592eba2d8ec9786ff
XS <0xqianyun+88@gmail.com>
2025-01-06 15:57:36 +0800

    Update jb.sh GPT-Gmini

--- jb.sh
+++ jb.sh
@@ -9,48 +9,113 @@ fi
 
 # 节点安装功能
 function install_node() {
-apt update
-apt install screen unzip -y
+    apt update
+    apt install screen unzip -y
 
-# 安装GO
-wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz -P /tmp/
-tar -C /usr/local -xzf /tmp/go1.22.1.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bashrc
-export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-go version
+    # 安装GO
+    wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz -P /tmp/
+    if [ $? -ne 0 ]; then
+        echo "下载Go安装包失败，请检查网络连接"
+        return 1
+    fi
 
-wget https://github.com/spectre-project/spectred/releases/download/v0.3.14/spectred-v0.3.14-linux-x86_64.zip
-unzip spectred-v0.3.14-linux-x86_64.zip
-cd ~/bin
-./spectred --utxoindex &
-SPECTRED_PID=$!
-sleep 3
-kill $SPECTRED_PID
-cd #HOME
-wget https://spectre-network.org/downloads/legacy/datadir2.zip
-unzip datadir2.zip -d ./.spectred/spectre-mainnet
-cd ~/bin
-screen -dmS spe bash -c './spectred --utxoindex'
+    tar -C /usr/local -xzf /tmp/go1.22.1.linux-amd64.tar.gz
+    if [ $? -ne 0 ]; then
+      echo "解压Go安装包失败"
+      return 1
+    fi
+    
+    echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bashrc
+    export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+    go version
+    if [ $? -ne 0 ]; then
+        echo "Go安装失败或无法执行"
+        return 1
+    fi
 
-read -p "请输入挖矿钱包地址: " wallet_addr
+    #创建~/bin目录
+    mkdir -p ~/bin
+    
+    wget https://github.com/spectre-project/spectred/releases/download/v0.3.14/spectred-v0.3.14-linux-x86_64.zip
+    if [ $? -ne 0 ]; then
+        echo "下载 spectred 二进制文件失败"
+        return 1
+    fi
+    
+    unzip spectred-v0.3.14-linux-x86_64.zip
+    if [ $? -ne 0 ]; then
+        echo "解压 spectred 二进制文件失败"
+        return 1
+    fi
+    
+    mv spectred ~/bin/
+    
+    cd ~/bin
+    
+    
+    ./spectred --utxoindex &
+    SPECTRED_PID=$!
+    sleep 3
+    kill $SPECTRED_PID
+    cd #HOME
+    wget https://spectre-network.org/downloads/legacy/datadir2.zip
+    if [ $? -ne 0 ]; then
+        echo "下载数据目录失败"
+        return 1
+    fi
+    
+    unzip datadir2.zip -d ./.spectred/spectre-mainnet
+    if [ $? -ne 0 ]; then
+        echo "解压数据目录失败"
+        return 1
+    fi
+    cd ~/bin
+    screen -dmS spe bash -c './spectred --utxoindex'
+    if [ $? -ne 0 ]; then
+        echo "启动 spectred 失败"
+        return 1
+    fi
 
-read -p "请输入挖矿CPU核心数: " cpu_core
+
+    read -p "请输入挖矿钱包地址: " wallet_addr
+
+    read -p "请输入挖矿CPU核心数: " cpu_core
+
+    # 检查cpu_core是否为整数
+    if ! [[ "$cpu_core" =~ ^[0-9]+$ ]]; then
+        echo "CPU核心数必须为整数"
+        return 1
+    fi
 
 
-screen -dmS spewa bash -c "./spectreminer --miningaddr='$wallet_addr' --workers='$cpu_core' --rpcserver=spr.tw-pool.com:14001"
+    screen -dmS spewa bash -c "./spectreminer --miningaddr='$wallet_addr' --workers='$cpu_core' --rpcserver=spr.tw-pool.com:14001"
+    if [ $? -ne 0 ]; then
+      echo "启动挖矿程序失败"
+      return 1
+    fi
 
-echo "====================== 安装完成 请使用screen -r spe 查看运行情况 ==========================="
+    echo "====================== 安装完成 请使用screen -r spe 查看运行情况 ==========================="
 
 }
 
 function miner() {
-cd ~/bin
+    cd ~/bin
 
-read -p "请输入挖矿钱包地址: " wallet_addr
-read -p "请输入挖矿CPU核心数: " cpu_core
+    read -p "请输入挖矿钱包地址: " wallet_addr
+    read -p "请输入挖矿CPU核心数: " cpu_core
 
-screen -dmS spewa bash -c "./spectreminer --miningaddr='$wallet_addr' --workers='$cpu_core' --rpcserver=spr.tw-pool.com:14001"
-echo "====================== 启动挖矿节点完成 请使用screen -r spewa 查看运行情况 ==========================="
+    # 检查cpu_core是否为整数
+    if ! [[ "$cpu_core" =~ ^[0-9]+$ ]]; then
+        echo "CPU核心数必须为整数"
+        return 1
+    fi
+    
+    screen -dmS spewa bash -c "./spectreminer --miningaddr='$wallet_addr' --workers='$cpu_core' --rpcserver=spr.tw-pool.com:14001"
+    if [ $? -ne 0 ]; then
+      echo "启动挖矿程序失败"
+      return 1
+    fi
+    echo "====================== 启动挖矿节点完成 请使用screen -r spewa 查看运行情况 ==========================="
 }
 # 主菜单
 function main_menu() {
